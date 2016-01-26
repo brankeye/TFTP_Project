ded3bd8bebd9956598769327a083439c7d22892f
Brandon Keyes <brankeye@gmail.com>
2016-01-26 16:21:33 -0500

    Implemented parse operation in PacketManager.

--- src/PacketManager.java
+++ src/PacketManager.java
@@ -3,4 +3,12 @@ public class PacketManager {
 	// TODO: handle encoding/decoding of packets
 	// TODO: read from packets
 	private boolean quiet_mode_enabled = true;
+	
+	public static Operation getOperation(byte[] data) {
+		if(data == null || data.length < 2) { return Operation.INVALID; }
+		for(int i = 1; i < Operation.values().length; ++i) {
+			if(data[1] == i) { return Operation.values()[i]; }
+		}
+		return Operation.INVALID;
+	}
 }
