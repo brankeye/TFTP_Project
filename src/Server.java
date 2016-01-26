8e97a38b278813c84060768d1d931b8841f1a934
Jason Bromfield <tehblackboy@gmail.com>
2016-01-25 22:26:41 -0500

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,27 @@
 /*For each RRQ, the server should respond with DATA block 1 and 0 bytes of data (no file I/O). 
 For each WRQ the server should respond with ACK block 0.
 
-As noted earlier, the server must be multithreaded. For this iteration, each newly created client connection thread should terminate after it sends the appropriate acknowledgment to the client that requested the connection.
+As noted earlier, the server must be multithreaded. 
+For this iteration, each newly created client connection thread should terminate after it sends 
+the appropriate acknowledgment to the client that requested the connection.
 
-Also, there must be a nice way to shut down both your server and your client. CRTL-C is NOT a nice way!*/
+Also, there must be a nice way to shut down both your server and your client. CRTL-C is NOT a nice way!
+extend the server program to support steady-state file transfer.
+Add steady-state file transfer capability to the client and server code developed in Iteration 0
+As in Iteration 0, your server should create a new client connection thread for each connection with a client. 
+You can have additional threads in the client and server, as long as you can justify them.
+*/
 public class Server {
-	
+	DatagramSocket receiveSocket = new DatagramSocket();
 	//sends and receives messages
 	public void sendReceive(){
-		//receive
+		while(true){
+			//receive
 		
-		//process
+			//process
 		
-		byte[] res = (data[1] == 1 ? new byte[] { 0, 3, 0, 1 } : new byte[] { 0, 4, 0, 0 });
+			byte[] res = (data[1] == 1 ? new byte[] { 0, 3, 0, 1 } : new byte[] { 0, 4, 0, 0 });
+		}
 	}
 	
 	public static void main(String[] args) {
