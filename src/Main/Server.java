d4662d345eb8d556bba40fef322a19a5a5e39bb4
Jason Bromfield <tehblackboy@gmail.com>
2016-01-27 01:49:14 -0500

    added private class  that implements Runnable
    Added a private class that implements Runnable to make it multithreaded. If anyone comes up with a better name feel free to rename it. It's a work in progress. A constructor for Server may or may not be necessary. I will look into this further.

--- src/Main/Server.java
+++ src/Main/Server.java
@@ -3,25 +3,20 @@ import General.Config;
 import General.NetworkConnector;
 import General.PacketReader;
 
-/*For each RRQ, the server should respond with DATA block 1 and 0 bytes of data (no file I/O). 
-For each WRQ the server should respond with ACK block 0.
-
-As noted earlier, the server must be multithreaded. 
+/*
 For this iteration, each newly created client connection thread should terminate after it sends 
 the appropriate acknowledgment to the client that requested the connection.
 
-Also, there must be a nice way to shut down both your server and your client. CRTL-C is NOT a nice way!
 extend the server program to support steady-state file transfer.
 Add steady-state file transfer capability to the client and server code developed in Iteration 0
-As in Iteration 0, your server should create a new client connection thread for each connection with a client. 
 You can have additional threads in the client and server, as long as you can justify them.
 */
 public class Server {
-	
 	// can send and receive packets (is meant to replace manual sockets!)
 	NetworkConnector networkConnector;
 	PacketReader     packetReader;
 	
+	//may or may not need this, will look further into this
 	public Server() {
 		networkConnector = new NetworkConnector(Config.SERVER_PORT, true);
 		packetReader     = new PacketReader("Server");
@@ -30,18 +25,35 @@ public class Server {
 	//sends and receives messages
 	public void sendReceive(){
 		while(true){
-			//receive
-		
-			//process
-		
-			//byte[] res = (data[1] == 1 ? new byte[] { 0, 3, 0, 1 } : new byte[] { 0, 4, 0, 0 });
+			//if(wantToStop) break; //there must be a nice way to shut down your server
+			DatagramPacket receivedPacket = networkConnector.receive();
+			/*if(receivedPacket == null){
+				System.out.println("Server: Waiting for a request...");
+			}*/
+			//else{
+				Splitter splitter = new Splitter(networkConnector.receive());
+				new Thread(splitter).start();
+			//
 		}
 	}
 	
 	public static void main(String[] args) {
 		Server server = new Server();
 		server.sendReceive();
-		// TODO Auto-generated method stub
 
+	}
+	
+	private class Splitter implements Runnable{
+		DatagramPacket datagramPacket;
+		public Splitter(DatagramPacket receivedPacket){
+			datagramPacket = receivedPacket;
+		}
+		@Override
+		public void run() {
+			packetReader.readReceivePacket(datagramPacket);
+			byte[] data = datagramPacket.getData();
+			byte[] res = (data[1] == 1 ? new byte[] { 0, 3, 0, 1 } : new byte[] { 0, 4, 0, 0 });
+			
+		}
 	}
 }
