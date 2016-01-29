165e7a3c4a6083378e02ef94599a1c1749d43dcd
IMaz6 <ihtisham-6@hotmail.com>
2016-01-29 04:28:03 -0500

    Updated with linking server - client

--- src/Main/ErrorSimulator.java
+++ src/Main/ErrorSimulator.java
@@ -1,4 +1,6 @@
 package Main;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -20,9 +22,9 @@ public class ErrorSimulator {
 	private int         serverPort;
 	
 	public ErrorSimulator() {
-		clientConnector     = new NetworkConnector(Config.ERR_SIM_PORT, true);
-		serverConnector     = new NetworkConnector();
-		packetReader        = new PacketReader("ErrorSim");
+		clientConnector  = new NetworkConnector(Config.ERR_SIM_PORT, true);
+		serverConnector  = new NetworkConnector();
+		packetReader     = new PacketReader("ErrorSim");
 		
 		try {
 			serverAddress = InetAddress.getByName(Config.SERVER_ADDRESS);
@@ -33,8 +35,71 @@ public class ErrorSimulator {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ErrorSimulator es = new ErrorSimulator();
+		while(true){
+			es.link();
+		}
 
 	}
+	
+	public void link(){
+		//TODO: Could split try-catch blocks to multiple ones
+		try{
+			//Create packet of max filled packet
+			byte dataClient[] = new byte[516];
+			DatagramPacket dpClient = new DatagramPacket(dataClient, dataClient.length);
+			
+			//Receive packet from client
+			//TODO: Add functionality for multiple clients 
+			dpClient = clientConnector.receive();
+			System.out.println("Client requesting from: " + dpClient.getAddress() + ":" + dpClient.getPort());
+			System.out.println("Client sent:" + dpClient.getData());
+			System.out.println("Client String:" +  byteToString(dpClient.getData(), dpClient.getLength()));
+		
+	
+			//-- ADD CALLS TO ERROR FUNCTION ON PACKETS HERE -- 
+			
+			
+			//--
+			
+			//Send packet to server
+			System.out.println("Sending to Server");
+			serverConnector.send(dpClient);
+			
+			//Receive the response from server
+			byte dataServer[] = new byte[516];
+			DatagramPacket dpServer = new DatagramPacket(dataServer, dataServer.length);
+			dpServer = serverConnector.receive();
+			
+			System.out.println("Server responded with:");
+			System.out.println("Response bytes: " + dpServer.getData());
+				
+			//Send server's response to client
+			clientConnector.send(dpServer);
+		}
+	
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+		//convert bytes to string to display --- for future use
+	public String byteToString(byte[] b, int len){
+			String s = new String(b);  // Create new String Object and assign byte[] to it
+			try {
+				s = new String(b, "UTF-8");  // Best way to decode using "UTF-8"
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return s;
+		}
+		//Error creation  --- for future use
+	public void createError(int level){
+		}
+		}
+	
 
-}
+	
+	
+
+
