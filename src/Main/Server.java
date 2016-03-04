86761e856c66cff8b09a649af4303632cff28446
Jason Bromfield <tehblackboy@gmail.com>
2016-03-04 17:36:53 -0500

    Update Server.java

--- src/Main/Server.java
+++ src/Main/Server.java
@@ -27,7 +27,7 @@ public class Server {
 
 	// may or may not need this, will look further into this
 	public Server() {
-		networkConnector = new NetworkConnector(Config.SERVER_PORT, true, 0);
+		networkConnector = new NetworkConnector(Config.SERVER_PORT, true);
 		shutdownHandler  = new ShutdownHandler();
 		
 	}
@@ -79,7 +79,7 @@ public class Server {
 		FileServer       fileServer;
 		public Splitter(DatagramPacket receivedPacket) {
 			datagramPacket           = receivedPacket;
-			threadedNetworkConnector = new NetworkConnector(10000);
+			threadedNetworkConnector = new NetworkConnector();
 			fileServer               = new FileServer(threadedNetworkConnector);
 			fileServer.setExpectedHost(datagramPacket.getPort());
 		}
